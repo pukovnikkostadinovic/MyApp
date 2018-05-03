fcac3a6c32429f1c9f822a925908de5ff85fd675
pukovnikkostadinovic <pukovnikkostadinovic@gmail.com>
2018-05-03 22:45:46 +0200

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+	<link rel="icon" href="https://www.freefavicon.com/freefavicons/objects/bubbling-potion-152-245369.png">
         <title>{{config('app.name','nestpNijeURefu')}}</title>
 
         <!-- Fonts -->
