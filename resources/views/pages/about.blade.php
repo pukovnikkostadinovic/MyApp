30e1a8ecabaee200e961b845b0ae7e4747d9c781
pukovnik <pukovnikkostadinovic@gmail.com>
2018-05-03 09:47:48 +0200

    light

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
 <h1>This is about page</h1>
+<button type="button" class="btn btn-primary" onclick="<?php $light = 10 ?>" >Primary</button>
 <h1>{{$light}}</h1>
 @endsection
