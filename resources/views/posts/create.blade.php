d356d5be523d75a963d098826eb9d9b3be59d6cb
pukovnik <pukovnikkostadinovic@gmail.com>
2018-05-04 12:45:10 +0200

    sadhsk

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,5 +3,10 @@
 @section('content')
 
 <h3>Create post</h3>
-
+{!! Form::open(['action'=>'PostsController@store','method'=>'POST']) !!}
+    <div class="form-group">
+    {{Form::label('title,'Title')}}
+    {{Form::text('title','',['class'=>'from-control','placeholder'='Title'])}}
+    </div>
+{!! Form::close() !!}
 @endsection
