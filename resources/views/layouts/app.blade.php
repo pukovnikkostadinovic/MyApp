b5c628b3108e9bd2a81e329250059050b512b328
Pukovnik <pukovnikkostadinovic@gmail.com>
2018-05-10 13:25:05 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,5 +33,11 @@
             @yield('content')
         </div>
     </div>
+    
+    
+     <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
 </body>
 </html>
