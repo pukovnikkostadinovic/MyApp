3c23c17cddea0bce4d54a544478391ea7b085d88
Pukovnik <pukovnikkostadinovic@gmail.com>
2018-05-11 09:48:45 +0200

    Update navbar.blade.php

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -30,6 +30,9 @@
                document.getElementById('logout-form').submit();">
             {{ __('Logout') }}
             </a>
+            <a class="dropdown-item" href="/posts/create">
+            Create post
+            </a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                @csrf
             </form>
