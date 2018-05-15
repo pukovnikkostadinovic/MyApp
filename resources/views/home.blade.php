307e33feda15040aa8a19a20910bd5a68423fb88
pukovnik <pukovnikkostadinovic@gmail.com>
2018-05-15 12:00:25 +0200

    haha

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,6 +26,7 @@
                     <td>{{$post->title}}</td>
                     <td><a href="/posts/{{$post->id}}/edit" class="btn btn-info">Edit</a></td>
                     <td>
+                    {!!Form::open(['action'=>['PostsController@destroy',$post->id], 'method'=>'POST', 'class'=>'pull-right'])!!}
                         {{Form::hidden('_method', 'DELETE')}}
                         {{Form::submit('Delete', ['class'=>'btn btn-danger'])}}
                         {!!Form::close()!!}
