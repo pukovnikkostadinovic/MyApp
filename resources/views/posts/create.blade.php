8e41af3d49b913b71a1272544fb3597fd46b366e
pukovnik <pukovnikkostadinovic@gmail.com>
2018-05-15 13:22:02 +0200

    file upload

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <h3>Create post</h3>
-{!! Form::open(['action'=>'PostsController@store','method'=>'POST']) !!}
+{!! Form::open(['action'=>'PostsController@store','method'=>'POST', 'enctype'=>'multipart/data']) !!}
     <div class="form-group">
     {{Form::label('title','Title')}}
     {{Form::text('title','',['class'=>'form-control','placeholder'=>'Title'])}}
@@ -12,6 +12,9 @@
     <div class="form-group">
     {{Form::label('body','Body')}}
     {{Form::textarea('body','',['id'=>'article-ckeditor','class'=>'form-control','placeholder'=>'Body Text'])}}
+    </div>
+    <div class="form-group">
+    	{{Form::file('cover_image')}}
     </div>
 	{{Form::submit('Submit',['class'=>'btn btn-primary'])}}
 {!! Form::close() !!}
